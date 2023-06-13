c2a4a4cb3874c92892581609cfa816620a34bd8f
Adam Brutsaert <brutsaertadam@yahoo.fr>
2023-06-13 20:27:11 +0200

    [+] Added basic argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 use beltex::{Solver};
+use clap;
 
 fn main() {
-    let mut solver = Solver::new(&[1, 2, 3, 4, 5, 6]);
+    let command = clap::Command::new("prog")
+        .arg(clap::Arg::new("base")
+            .short('b')
+            .long("base")
+            .value_delimiter(',')
+            .default_value("1,2,3,4,5,6,7,8,9"))
+        .arg(clap::Arg::new("target"))
+        .get_matches();
 
-    solver.iterate_until_get(26);
+    let base = command.get_many::<String>("base").unwrap().map(|v| v.parse().unwrap()).collect::<Vec<i32>>();
+    let target = command.get_one::<String>("target").unwrap().parse::<i32>().unwrap();
 
-    let lisps = solver.get_lisp(26);
-
-    for lisp in lisps {
-        println!("26 = {}", lisp);
+    let mut solver = Solver::new(base.as_slice());
+    solver.iterate_until_get(target);
+    for lisp in solver.get_lisp(target) {
+        println!("{target} = {lisp}");
     }
 }
